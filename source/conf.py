6077be4fd739e19b04172c16d1798562e65b57ad
Pushkar <121485926+dpshkr@users.noreply.github.com>
2025-06-24 20:59:10 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -26,3 +26,8 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme_options = {
+    'logo': '_static/Figures/iisclogo.jpg',
+    'github_user': 'dpshkr',
+    'github_repo': 'mythesis',
+}
