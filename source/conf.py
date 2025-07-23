a24b7568e88dbaaed907e9250936e17e6844aa75
Pushkar <121485926+dpshkr@users.noreply.github.com>
2025-07-24 00:26:02 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -24,14 +24,13 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'alabster'
 html_static_path = ['_static']
-'''
+
 html_theme_options = {
     'logo': 'Figures/iisclogo.png',
     'github_user': 'dpshkr',
     'github_repo': 'mythesis',
     'body_text_align':'justify',
-    'show_relbar_bottom': True,
 }
-'''
+
